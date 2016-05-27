6634db1c08168ce2bb45ce7b0f41e792fbf9b45f
edwardgao <gaochangyu92@gmail.com>
2016-05-27 13:52:44 +0300

    add Ros publisher provider

---
+++ core/src/main/java/com/state_machine/core/providers/RosPublisherProvider.java
@@ -0,0 +1,22 @@
+package com.state_machine.core.providers;
+
+import mavros_msgs.OverrideRCIn;
+import org.ros.node.ConnectedNode;
+import org.ros.node.topic.Publisher;
+
+/**
+ * Created by parallels on 5/17/16.
+ */
+public class RosPublisherProvider {
+    private Publisher<OverrideRCIn> overrideRCInPublisher;
+
+    public RosPublisherProvider(ConnectedNode node){
+        overrideRCInPublisher = node.newPublisher("/mavros/rc/override",OverrideRCIn._TYPE);
+    }
+
+    public Publisher<OverrideRCIn> getOverrideRCInPubliser(){
+        return overrideRCInPublisher;
+    }
+
+
+}
