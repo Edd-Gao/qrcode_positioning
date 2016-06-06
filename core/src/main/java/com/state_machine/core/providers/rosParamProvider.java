829063fe69b9347fff93ed3153c680e9555d7fc4
edwardgao <gaochangyu92@gmail.com>
2016-06-06 22:30:27 +0300

    fix compiling problem

--- core/src/main/java/com/state_machine/core/providers/rosParamProvider.java
+++ core/src/main/java/com/state_machine/core/providers/rosParamProvider.java
@@ -1,10 +1,12 @@
 package com.state_machine.core.providers;
 
+import com.state_machine.core.states.State;
 import com.state_machine.core.states.util.ErrorType;
 import com.state_machine.core.states.util.Failure;
 import mavros_msgs.ParamPullRequest;
 import mavros_msgs.ParamPullResponse;
 import mavros_msgs.SetModeResponse;
+import org.apache.commons.logging.Log;
 import org.ros.exception.RemoteException;
 import org.ros.node.ConnectedNode;
 import org.ros.node.parameter.*;
@@ -18,12 +20,12 @@ public class rosParamProvider {
     private ParameterTree params;
     RosServiceProvider rosServiceProvider;
     private ServiceClient<ParamPullRequest, ParamPullResponse> paramPullService;
-
+    private Log log;
     public rosParamProvider(ConnectedNode node,
-                            RosServiceProvider rosServiceProvider){
+                            RosServiceProvider rosServiceProvider,
+                            Log log){
         this.rosServiceProvider = rosServiceProvider;
         this.paramPullService = rosServiceProvider.getParamPullService();
-        try {
             ParamPullRequest request = paramPullService.newMessage();
             request.setForcePull(true);
             paramPullService.call(request, new ServiceResponseListener<ParamPullResponse>() {
@@ -34,12 +36,10 @@ public class rosParamProvider {
 
                 @Override
                 public void onFailure(RemoteException e) {
-                    log.warn("Failed to set mode", e);
-                    lastFailure = new Failure(ErrorType.ConnectionFailure, currentTime);
+                    rosParamProvider.this.log.warn("Failed to pull params", e);//Todo:I don't know wether this will work.
                 }
-            params = node.getParameterTree();
-
-        }
+            });
+        params=node.getParameterTree();
     }
 
     public double getAC_A(){return params.getDouble("/");}
