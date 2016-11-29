7408d9411ff008178836067318a13c23d0708818
Gao Changyu <gaochangyu@163.com>
2016-11-30 00:40:16 +0800

    update

--- scripts/position_broadcast.py
+++ scripts/position_broadcast.py
@@ -2,52 +2,54 @@
 
 
 import rospy
-from geometry_msgs.msg import Pose
 from geometry_msgs.msg import PoseStamped
 from gazebo_msgs.msg import ModelStates
 from std_msgs.msg import Header
 
-pub = rospy.Publisher('mavros/vision_pose/pose',PoseStamped,queue_size=1)
+pub = rospy.Publisher('mavros/vision_pose/pose', PoseStamped, queue_size=1)
 
-sequence = 0;
+sequence = 0
 i = 0
 
-def gazeboCallBack(data):
-    '''
 
-    '''
-    #print data.pose[1].position
+def gazeboCallBack(data, model_name):
+    # print data.pose[1].position
     global sequence
     global i
 
-    i = i + 1
+    i += 1
 
     if i % 5 == 0:
-        pose = data.pose[1]
+
+        pose = data.pose[data.name.index(model_name)]
 
         temp = pose.position.x
         pose.position.x = -pose.position.y
 
         pose.position.y = temp
-        
+
         header = Header()
-        
+
         header.seq = sequence
-        sequence = sequence + 1
+        sequence += 1
         header.stamp = rospy.Time.now()
 
-        pub.publish(header,pose)
+        pub.publish(header, pose)
 
 
 def positionBroadcast():
-       
-    rospy.init_node('position_broadcast',anonymous=True)
+    rospy.init_node('position_broadcast', anonymous=True)
 
-    rospy.Subscriber("/gazebo/model_states",ModelStates,gazeboCallBack)
+    while not rospy.has_param('~model_name'):
+        pass
+
+    model_name = rospy.get_param('~model_name')
+
+    assert isinstance(model_name, str)
+    rospy.Subscriber("/gazebo/model_states", ModelStates, gazeboCallBack, model_name)
 
     rospy.spin()
 
 
 if __name__ == '__main__':
     positionBroadcast()
-
