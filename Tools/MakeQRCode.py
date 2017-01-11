14d517c2a25741c7f9cec181c3ab17f808d1141f
Gao Changyu <gaochangyu@163.com>
2017-01-11 19:30:41 +0800

    minor fix

--- Tools/MakeQRCode.py
+++ Tools/MakeQRCode.py
@@ -17,7 +17,7 @@ for i in range(rows):
     for j in range(lines):
         coordinate_x = i * (qrsize + margin)
         coordinate_y = j * (qrsize + margin)
-        version, rawsize, qrcodeIm = qrencode.encode(qrsize + "mm-" + str(coordinate_x/10) + "," + str(coordinate_y/10))
+        version, rawsize, qrcodeIm = qrencode.encode(str(qrsize) + "mm-" + str(coordinate_x/10) + "," + str(coordinate_y/10))
         qrcodeIm = qrcodeIm.resize((qrsize, qrsize))
         box = (j*qrsize + (j+1)*margin, i*qrsize + (i+1)*margin)
         result.paste(qrcodeIm, box)
