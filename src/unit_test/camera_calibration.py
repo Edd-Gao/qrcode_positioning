a891a3db15dcdf3e6ef8addd494e55bca71761bd
Gao Changyu <gaochangyu@163.com>
2017-03-06 19:37:00 +0800

    qrcode_positioning: add python camera calibration parser test.

---
+++ src/unit_test/camera_calibration.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+'''
+ name: camera_calibration.py
+ usage: used to test the camera calibration info parsers
+'''
+
+import camera_calibration_parsers as parser
+
+try:
+    name, info = parser.readCalibration('rpi_cam_v2_640_480.yml')
+except TypeError:
+    print "error reading calibrations."
